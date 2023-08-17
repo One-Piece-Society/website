3d20ccb7435dbbb41539baa0e90a2f632836d47f
HARI-PRMD <haripramodb@gmail.com>
2023-08-17 19:47:57 +1000

    Updated Event Cards for xs screens

--- src/components/EventCardMini.tsx
+++ src/components/EventCardMini.tsx
@@ -10,7 +10,7 @@ const EventCardMini: React.FC<Props> = ({
   title,
 }) => {
   return (
-    <div className="flex h-fit w-72 snap-x snap-center flex-col overflow-clip rounded-2xl border-2  border-white/40 drop-shadow-md backdrop-blur md:h-full md:w-full md:flex-row">
+    <div className="flex h-fit w-56 snap-x snap-center flex-col overflow-clip rounded-2xl border-2 border-white/40  drop-shadow-md backdrop-blur xs:w-72 md:h-full md:w-full md:flex-row">
       <Image
         alt="event"
         src={image}
@@ -20,11 +20,17 @@ const EventCardMini: React.FC<Props> = ({
       />
       <div className="flex w-full flex-col justify-between border-l-0 border-t-2 border-white/40 bg-gradient-to-br from-white/10 to-white/20 text-white md:w-2/3 md:border-l-2 md:border-t-0">
         <div className="p-4">
-          <h1 className="text-2xl font-semibold md:text-4xl">{title}</h1>
-          <p className="py-2 ">{description.slice(0, 180)} ...</p>
+          <h1 className="text-xl font-semibold xs:text-2xl md:text-4xl">
+            {title}
+          </h1>
+          <p className="py-2 text-sm xs:text-base">
+            {description.slice(0, 180)} ...
+          </p>
         </div>
         <div className="h-fit rounded-b-2xl border-t-2 border-white/40 bg-gradient-to-tl from-white/10 to-white/20 md:rounded-b-none md:rounded-br-2xl">
-          <div className="p-4">Date: {date.toDateString()}</div>
+          <p className="p-4 text-sm md:text-base">
+            Date: {date.toDateString()}
+          </p>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 } satisfies Config;
