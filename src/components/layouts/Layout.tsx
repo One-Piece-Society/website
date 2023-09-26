659ee15cd815ce79b8d7e178702b67e9992bdab3
HARI-PRMD <haripramodb@gmail.com>
2023-09-26 10:58:49 +1000

    Moved SEO component Call to Index Page

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,36 +1,22 @@
-import { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import SEO from "./SEO";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<Props> = ({ children }) => {
-  const [eventId, setEventId] = useState("");
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get("event")) {
-      const id = params.get("event");
-      setEventId(id ?? "");
-    }
-  }, []);
-
   return (
-    <>
-      <SEO eventId={eventId} />
-      <main
-        className="flex h-full w-full flex-col
+    <main
+      className="flex h-full w-full flex-col
       justify-between bg-primary-red text-white"
-      >
-        <div>
-          <Header />
-          {children}
-        </div>
-        <Footer />
-      </main>
-    </>
+    >
+      <div>
+        <Header />
+        {children}
+      </div>
+      <Footer />
+    </main>
   );
 };
 
--- src/components/layouts/SEO.tsx
+++ src/components/layouts/SEO.tsx
@@ -1,34 +1,19 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { DefaultSEO, baseUrl } from "~/util/SEO";
-import { api } from "~/util/api";
+import { baseUrl } from "~/util/SEO";
 import { type SEOProps } from "~/util/types";
 
-const SEO: React.FC<{ eventId?: string }> = ({ eventId }) => {
-  const [seoData, setSeoData] = useState<SEOProps>(DefaultSEO);
-  const event = api.event.getEvent.useQuery({ id: eventId });
+type Props = { data: SEOProps & { eventId?: string } };
 
-  useEffect(() => {
-    if (event.isSuccess && typeof window !== "undefined") {
-      setSeoData({
-        title: event.data.title,
-        description: event.data.description + " | UNSW OPSoc",
-        image: event.data.image ?? "",
-        url: baseUrl + "?event=" + eventId,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [event.isSuccess]);
-
+const SEO: React.FC<Props> = ({ data }) => {
   const DEFAULT_IMAGE =
     "https://media.discordapp.net/attachments/956904556132962334/957112846716661790/One_Piece_Logo.png";
   const favicon = `${baseUrl}/static/favicon.ico`;
 
   return (
     <Head>
-      <title>{seoData.title}</title>
-      <meta name="title" content={seoData.title} />
-      <meta name="description" content={seoData.description} />
+      <title>{data.title}</title>
+      <meta name="title" content={data.title} />
+      <meta name="description" content={data.description} />
       <meta
         name="keywords"
         content="unsw, UNSW, one piece, Luffy, manga, anime, Sydney"
@@ -39,19 +24,19 @@ const SEO: React.FC<{ eventId?: string }> = ({ eventId }) => {
       <link rel="icon" href={favicon} />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={seoData.url} />
-      <meta property="og:title" content={seoData.title} />
-      <meta property="og:description" content={seoData.description} />
-      <meta property="og:image" content={seoData.image ?? DEFAULT_IMAGE} />
+      <meta property="og:url" content={data.url} />
+      <meta property="og:title" content={data.title} />
+      <meta property="og:description" content={data.description} />
+      <meta property="og:image" content={data.image ?? DEFAULT_IMAGE} />
       <meta property="og:image:width" content="320" />
       <meta property="og:image:height" content="320" />
       <meta property="og:image:type" content="image/png" />
 
       <meta name="twitter:card" content="summary" />
-      <meta property="twitter:url" content={seoData.url} />
-      <meta property="twitter:title" content={seoData.title} />
-      <meta property="twitter:description" content={seoData.description} />
-      <meta property="twitter:image" content={seoData.image ?? DEFAULT_IMAGE} />
+      <meta property="twitter:url" content={data.url} />
+      <meta property="twitter:title" content={data.title} />
+      <meta property="twitter:description" content={data.description} />
+      <meta property="twitter:image" content={data.image ?? DEFAULT_IMAGE} />
       <meta name="twitter:image:alt" content="Logo" />
     </Head>
   );
